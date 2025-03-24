2c52bfdc25c7d58e681154477b5a9ae55a8ba4a9
librekcesar <140665209+librekcesar@users.noreply.github.com>
2025-03-24 15:42:42 -0600

    Add files via upload

---
+++ centro_ideal.js
@@ -0,0 +1,8 @@
+var centro_ideal = {
+"type": "FeatureCollection",
+"name": "centro_ideal",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "country": "MEX", "locality": "Gustavo A. Madero", "state": "Ciudad de México", "zonificacion": "GAM_14", "catstationid": "1", "station_code": "SPMX15", "zipcode": "07239", "region": "SP_Metro", "station_name": "Pantaco", "id": "810", "coverage_type": "Local", "cobertura_diferenciada": "No Diferenciado", "cluster_diferenciado": "ZM", "polygon": "1", "neighborhood": null, "fid": "1156", "sameday_SameDay": "Si", "tiempo_1": "> 25 min and < 45 min", "sameday_tiempo_1": "> 25 min and < 45 min" }, "geometry": { "type": "Point", "coordinates": [ -99.111313298354105, 19.413251488460421 ] } }
+]
+}
---
+++ polanco.js
@@ -0,0 +1,8 @@
+var polanco_ubi = {
+"type": "FeatureCollection",
+"name": "polanco",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": 1, "nombre": "POLANCO" }, "geometry": { "type": "Point", "coordinates": [ -99.181739939856257, 19.431422634097135 ] } }
+]
+}
