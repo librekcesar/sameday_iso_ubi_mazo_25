4f4416d40277ebe534409990dfcd2b56cfb4474d
librekcesar <140665209+librekcesar@users.noreply.github.com>
2025-03-26 11:59:23 -0600

    Add files via upload

---
+++ tulti.js
@@ -0,0 +1,8 @@
+var tulti = {
+"type": "FeatureCollection",
+"name": "tulti",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": 1, "nombre": "Tultitlan" }, "geometry": { "type": "Point", "coordinates": [ -99.141950362979799, 19.640286096590483 ] } }
+]
+}
