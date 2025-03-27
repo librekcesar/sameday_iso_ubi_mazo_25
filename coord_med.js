312c08fa7e33106afecbc8aba024ec509da79288
librekcesar <140665209+librekcesar@users.noreply.github.com>
2025-03-27 08:03:51 -0600

    Add files via upload

---
+++ coord_med.js
@@ -0,0 +1,8 @@
+var media = {
+"type": "FeatureCollection",
+"name": "coord_media_muestra",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "MEAN_X": -99.146005344841342, "MEAN_Y": 19.435803781138382 }, "geometry": { "type": "Point", "coordinates": [ -99.146005344841342, 19.435803781138382 ] } }
+]
+}
